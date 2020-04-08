8a1e5913469e6b3030cce5579b8af76f6d55d118
Spencer Ke <kespencer88@gmail.com>
2020-04-08 00:44:22 -0700

    added arror handling and error pages

---
+++ 500.php
@@ -0,0 +1,44 @@
+<?php
+
+?>
+<!Doctype HTML>
+
+<html>
+
+<head>
+    <title>Talk About Turtles</title>
+    <link rel="stylesheet" href="./CSS/navbar.css">
+    <link rel="stylesheet" href="./CSS/blogpage.css">
+</head>
+
+<div id="navbar">
+    <div id="logo">
+        <a href="./main.php"></a>
+            <img src="./CSS/images/Turtle.png">
+        </a>
+    </div>
+    <div id="title">
+        <h1>Talk About Turtles</h1>
+    </div>
+    <div id="searchbar">
+        <form action="./search.php" method="POST">
+            <input id="searchfield" name="search" type="text"/>
+            <button type ="submit" id="searchbtn"><img id="searchimg" src="./CSS/images/search.png"></button>
+        </form>
+    </div>
+    <div id="login">
+        <?php if (empty(isLoggedIn())){ ?>
+            <a class="linkbutton" href="./pages/login.php">Login/Signup</a>
+        <?php } else { ?>
+            <form action="./pages/accountManage.php" method="POST">
+                <input type="hidden" name="logged_in" value="<?php echo isLoggedIn();?>"/>
+                <button type="submit" class="linkbutton">Manage Account</button>
+            </form>
+        <?php } ?>
+    </div>
+</div>
+
+<body>
+    <header> <h1> Error 500: Internal Server Error </h1> </header>
+    <h2>Something's wrong on our end...</h2>
+</body>
