cc528b83ffbf88e976abe8859f84cf20bfa2a4b7
tyreln <tyrelnarciso@hotmail.com>
2020-04-09 21:08:12 -0700

    asdf

--- pages/blogCreate.php
+++ pages/blogCreate.php
@@ -17,6 +17,7 @@ require "../vendor/autoload.php";
 
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
+  
   $user=$_SESSION['username'];
   $user_ids = $query->getUserID($user);
   if(!empty($user_ids)){
@@ -33,12 +34,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
       //reference: https://www.w3schools.com/php/php_file_upload.asp?fbclid=IwAR0e9PACag0wZ_azGC9gjEYun27THoWOMt5OORmb9diMT22X48m5nGg2vKs
         $target_dir = "../uploads/";
-        $target_file = $target_dir . basename($_FILES[$_POST["fileToUpload"]]["name"]);
+        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
         $uploadOk = 1;
         $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
         // Check if image file is a actual image or fake image
         if(isset($_POST["submit"])) {
-            $check = getimagesize($_FILES[$_POST["fileToUpload"]]["tmp_name"]);
+            $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
             if($check !== false) {
                 echo "File is an image - " . $check["mime"] . ".";
                 $uploadOk = 1;
@@ -68,9 +69,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             echo "Sorry, your file was not uploaded.";
         // if everything is ok, try to upload file
         } else {
+            ECHO "ADSFASD;LFKASD;FJLKADSF";
             if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
                 echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-                header("location: main.php");
+              
             } else {
                 echo "Sorry, there was an error uploading your file.";
             }
